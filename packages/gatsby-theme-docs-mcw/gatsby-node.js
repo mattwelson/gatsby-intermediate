95431554ca0463b7194f2becf050b4e23d38ba4e
Matt Welson <matthew.welson@gmail.com>
2019-10-22 17:08:15 +1300

    feat(theme-dev): add custom node

--- packages/gatsby-theme-docs-mcw/gatsby-node.js
+++ packages/gatsby-theme-docs-mcw/gatsby-node.js
@@ -1,14 +1,74 @@
-const path = require('path');
-const fs = require('fs');
-const mkdirp = require('mkdirp');
-const withDefaults = require('./utils/default-options');
+const path = require('path')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const withDefaults = require('./utils/default-options')
 
 exports.onPreBootstrap = ({ store }, options) => {
-  const { program } = store.getState();
-  const { contentPath } = withDefaults(options);
-  const dir = path.join(program.directory, contentPath);
+  const { program } = store.getState()
+  const { contentPath } = withDefaults(options)
+  const dir = path.join(program.directory, contentPath)
 
   if (!fs.existsSync(dir)) {
-    mkdirp.sync(dir);
+    mkdirp.sync(dir)
   }
-};
+}
+
+exports.createSchemaCustomization = ({ actions }, options) => {
+  actions.createTypes(`
+    type DocsPage implements Node @dontInfer {
+      id: ID!
+      title: String!
+      path: String!
+      updated: Date! @dateformat
+      body: String!
+    }
+  `)
+}
+
+exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+  const { basePath } = withDefaults(options)
+  const parent = getNode(node.parent)
+
+  // only work on MDX files loaded by this theme
+  if (
+    node.internal.type !== 'Mdx' ||
+    parent.sourceInstanceName !== 'gatsby-theme-docs-mcw'
+  )
+    return
+
+  // handle index.mdx as index file
+  const pageName = parent.name !== 'index' ? parent.name : ''
+
+  actions.createNode({
+    id: createNodeId(`DocsPages-${node.id}`),
+    title: node.frontmatter.title || parent.name,
+    updated: parent.modifiedTime,
+    path: path.join('/', basePath, parent.relativeDirectory, pageName),
+    parent: node.id,
+    internal: {
+      type: 'DocsPage',
+      contentDigest: node.internal.contentDigest,
+    },
+  })
+}
+
+exports.createResolvers = ({ createResolvers }) => {
+  createResolvers({
+    DocsPage: {
+      body: {
+        type: 'String!',
+        resolve: (source, args, context, info) => {
+          // load the resolver for the mdx type 'body' field
+          const type = info.schema.getType('Mdx')
+          const mdxFields = type.getFields()
+          const resolver = mdxFields.body.resolve
+
+          const mdxNode = context.nodeModel.getNodeById({ id: source.parent })
+          return resolver(mdxNode, args, context, {
+            fieldName: 'body',
+          })
+        },
+      },
+    },
+  })
+}
